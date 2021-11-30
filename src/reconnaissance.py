b2cb4bcbeec73aab13bf74da0bc81135fec7884f
enzoBENITO <90310892+enzoBENITO@users.noreply.github.com>
2021-11-30 12:00:41 +0100

    Add files via upload

--- src/reconnaissance.py
+++ src/reconnaissance.py
@@ -12,5 +12,16 @@ def lecture_modeles(chemin_dossier):
 
 
 def reconnaissance_chiffre(image, liste_modeles, S):
-    pass
+    image_bin = image.binarisation(S)
+    image_loc=image_bin.localisation()
+    Sim_max=0
+    Sim_ind=0
 
+    for u in range (0, len(liste_modeles)):
+        im_bin4 = image_loc.resize( liste_modeles[u].H, liste_modeles[u].W )
+        Sim_im = im_bin4.similitude(liste_modeles[u])
+        if Sim_im > Sim_max:
+            Sim_max = Sim_im  
+            Sim_ind = u
+    
+    return Sim_ind     
